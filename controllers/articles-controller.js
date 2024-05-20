09f74b42939651b3856a72f661c765402af5a967
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-05-20 13:38:23 +0300

    modify controllers to ffetch dynamic data

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -1,28 +1,28 @@
 import "dotenv/config";
-import news from "../data/news.js";
-import todayNews from "../data/todayNews.js";
+import axios from "axios";
 
 const getArticles = async (req, res) => {
-  // const headers = {
-  //   "X-RapidAPI-Key": process.env.RAPID_API_KEY,
-  //   "X-RapidAPI-Host": process.env.RAPID_API_HOST,
-  // };
+  const headers = {
+    "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+    "X-RapidAPI-Host": process.env.RAPID_API_HOST,
+  };
 
   try {
-    // const articlesResponse = await axios.get(process.env.SPACE_NEWS_API_URL_2, {
-    //   headers,
-    // });
-    // const articles = articlesResponse.data;
+    const articlesResponse = await axios.get(process.env.SPACE_NEWS_API_URL_2, {
+      headers,
+    });
+    const articles = articlesResponse.data;
 
-    // const todayArticlesResponse = await axios.get(
-    //   process.env.SPACE_NEWS_API_URL,
-    //   {
-    //     headers,
-    //   }
-    // );
-    // const todayArticles = todayArticlesResponse.data;
+    const todayArticlesResponse = await axios.get(
+      process.env.SPACE_NEWS_API_URL,
+      {
+        headers,
+      }
+    );
+    const todayArticles = todayArticlesResponse.data;
+    console.log(todayArticles);
 
-    res.render("index.ejs", { news, todayNews });
+    res.render("index.ejs", { articles, todayArticles });
   } catch (error) {
     res.render("index.ejs", {
       error,
