2742a9df29d31c861223561178b5c69b885a67b9
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-07-21 19:54:30 +0300

    import path

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,7 @@
 import express from "express";
 import router from "../routes/routes.js";
 import serverless from "serverless-http";
+import path from "path";
 
 const app = express();
 // const port = 8000;
