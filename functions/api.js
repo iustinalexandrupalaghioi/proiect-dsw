1373ffc5612037c2d354b15c1281738a1d8fa9c5
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-07-21 20:18:00 +0300

    file path

--- functions/api.js
+++ functions/api.js
@@ -1,10 +1,16 @@
 import express from "express";
 import router from "../routes/routes.js";
 import serverless from "serverless-http";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 // const port = 8000;
 
+app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/api", router);
