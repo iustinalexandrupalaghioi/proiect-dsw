d218ed6a97b165539f3c9d47dba852a9092e0598
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-07-21 19:52:48 +0300

    path

--- functions/api.js
+++ functions/api.js
@@ -5,7 +5,7 @@ import serverless from "serverless-http";
 const app = express();
 // const port = 8000;
 
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 app.use(express.static("public"));
 app.use("/", router);
 
