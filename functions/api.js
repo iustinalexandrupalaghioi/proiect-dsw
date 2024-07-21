99ebea053aa87c2263db843e6b7dd12d37687ed7
iustinalexandrupalaghioi <alex.palaghioi@gmail.com>
2024-07-21 20:15:22 +0300

    delete path

--- functions/api.js
+++ functions/api.js
@@ -1,13 +1,11 @@
 import express from "express";
 import router from "../routes/routes.js";
 import serverless from "serverless-http";
-import path from "path";
 
 const app = express();
 // const port = 8000;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "../views"));
 app.use(express.static("public"));
 app.use("/api", router);
 
